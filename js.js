cec9a415f4301b2215d04e02a4e7d2a6762568e6
Oozy99 <jenifervarelas99@gmail.com>
2023-08-30 04:27:54 -0500

    actualizacion de contador

--- js.js
+++ js.js
@@ -6,45 +6,62 @@ const EMPATE = 0;
 const GANADOR = 1;
 const PERDEDOR = 2;
 
-
-
-const piedraBtn =document.getElementById("piedra");
-const papelBtn =document.getElementById("papel");
-const tijeraBtn =document.getElementById("tijera");
+const piedraBtn = document.getElementById("piedra");
+const papelBtn = document.getElementById("papel");
+const tijeraBtn = document.getElementById("tijera");
 const resultadoTexto = document.getElementById("texto_inicial");
-const img_maquina = document.getElementById("img_maquina");
+const imgMaquina = document.getElementById("img_maquina");
+const contadorJugador = document.getElementById("contador-jugador");
+const contadorMaquina = document.getElementById("contador-maquina");
 
-piedraBtn.addEventListener("click", ()=>{
- jugar(PIEDRA);
+let jugadorWins = 0;
+let maquinaWins = 0;
+
+piedraBtn.addEventListener("click", () => {
+    jugar(PIEDRA);
 });
 
-papelBtn.addEventListener("click", ()=>{
+papelBtn.addEventListener("click", () => {
     jugar(PAPEL);
 });
 
-tijeraBtn.addEventListener("click", ()=>{
+tijeraBtn.addEventListener("click", () => {
     jugar(TIJERA);
 });
 
-function jugar (usuariOpcion){
-  const machineOption = calMaquina();
-  const resultado =calResultado(usuariOpcion,machineOption);
-  img_maquina.src = "img/" + machineOption + ".png";
-
-
-switch (resultado){
-    case EMPATE:
-        resultadoTexto.innerHTML= " ¡¡¡HAS EMPATADO!!!";
-      
-        break;
+function jugar(usuariOpcion) {
+    const machineOption = calMaquina();
+    const resultado = calResultado(usuariOpcion, machineOption);
+    imgMaquina.src = "img/" + machineOption + ".png";
+    mostrarEleccion(usuariOpcion, machineOption);
+    switch (resultado) {
+        case EMPATE:
+            resultadoTexto.innerHTML = "¡¡¡HAS EMPATADO!!!";
+            break;
         case GANADOR:
-            resultadoTexto.innerHTML= " ¡¡¡HAS GANADO!!!";
-        break;
+            resultadoTexto.innerHTML = "¡¡¡HAS GANADO!!!";
+            jugadorWins++;
+            contadorJugador.textContent = jugadorWins;
+            break;
         case PERDEDOR:
-            resultadoTexto.innerHTML= " ¡¡¡HAS PERDIDO!!!";
-        break;
-
+            resultadoTexto.innerHTML = "¡¡¡HAS PERDIDO!!!";
+            maquinaWins++;
+            contadorMaquina.textContent = maquinaWins;
+            break;
+    }
 }
+
+
+
+function mostrarEleccion(eleccionJugador, eleccionMaquina) {
+    const textoJugador = document.getElementById("texto-jugador");
+    const textoMaquina = document.getElementById("texto-maquina");
+
+    textoJugador.textContent = eleccionJugador;
+    textoMaquina.textContent = eleccionMaquina;
+}
+
+
 
 function calMaquina(){
 const numero=Math.floor(Math.random() * 3);
@@ -57,9 +74,6 @@ switch (numero){
     case 2:
        return TIJERA;
 }
-
-}
-
 
 }
 
